1897938aa7e28499378268b75042e5b5b8da3018
Paige <unlovablep@gmail.com>
2024-06-14 12:22:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const http = require("http")
 const url = require("url")
 const path = require("path")
 const fs = require("fs")
-const usedir = path.join(process.cwd(), "./public/")
+const usedir = path.join(process.cwd(), "./docs/")
 const port = process.argv[2] || 8888
 
 if (!fs.statSync(usedir).isDirectory()) {
